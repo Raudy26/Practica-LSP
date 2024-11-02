ea6fca0d34541d5be5a1403308a75200fcc54125
Lenovo <Lenovo@DESKTOP-MFJHMIE>
2024-11-02 12:05:09 -0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+abstract class FiguraGeometrica
+{
+    public abstract double CalcularArea();
+}
+
+class Rectangulo : FiguraGeometrica
+{
+    public double Base { get; set; }
+    public double Altura { get; set; }
+
+    public override double CalcularArea()
+    {
+        return Base * Altura;
+    }
+}
+
+class Cuadrado : FiguraGeometrica
+{
+    public double Lado { get; set; }
+
+    public override double CalcularArea()
+    {
+        return Lado * Lado;
+    }
+}
+
+// Programa principal
+class Program
+{
+    static void Main(string[] args)
+    {
+        FiguraGeometrica rectangulo = new Rectangulo { Base = 5, Altura = 4 };
+        FiguraGeometrica cuadrado = new Cuadrado { Lado = 4 };
+
+        Console.WriteLine("Área del Rectángulo: " + rectangulo.CalcularArea());
+        Console.WriteLine("Área del Cuadrado: " + cuadrado.CalcularArea()); 
+    }
+}
